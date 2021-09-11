d5e947b9ff8f94c24baf8c3deb9dd05defc0a653
mohamadjarmak@gmail.com <Mj314159265!>
2021-09-11 12:35:33 +0200

    improved read me

--- spammer_selenium.py
+++ spammer_selenium.py
@@ -1,6 +1,6 @@
 # geckodriver exe from https://github.com/mozilla/geckodriver/releases/tag/v0.29.1
-# heroku buildpacks:add -a instagram-spammer-prod --index 1 https://github.com/heroku-buildpack-chromedriver
-# heroku buildpacks:add -a instagram-spammer-prod --index 2 https://github.com/heroku-buildpack-chromedriver
+# heroku buildpacks:add -a instagram-spammer-prod --index 1 https://github.com/heroku/heroku-buildpack-chromedriver
+# heroku buildpacks:add -a instagram-spammer-prod --index 2 https://github.com/heroku/heroku-buildpack-google-chrome
 # heroku config:set -a instagram-spammer-prod GOOGLE_CHROME_BIN=/app/.apt/usr/bin/google_chrome
 # heroku config:set -a instagram-spammer-prod CHROMEDRIVER_PATH=/app/.chromedriver/bin/chromedriver
 
