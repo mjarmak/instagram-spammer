c237fd0c6cb2577b9d3a5c0dbb4fd430215fbba9
mohamadjarmak@gmail.com <Mj314159265!>
2021-09-11 13:44:30 +0200

    tweak

--- spammer_selenium_local.py
+++ spammer_selenium_local.py
@@ -10,9 +10,19 @@ print("Starting...", file=sys.stderr)
 
 # os.chmod('./chromedriver.exe', 0o755)
 options = webdriver.ChromeOptions()
-# options.add_argument("--headless")
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
+
+options.add_argument("--headless")
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--allow-running-insecure-content')
+options.add_argument("--disable-extensions")
+options.add_argument("--start-maximized")
+options.add_argument("--proxy-server='direct://'")
+options.add_argument("--proxy-bypass-list=*")
+options.add_argument('--disable-dev-shm-usage')
+
 
 browser = webdriver.Chrome(options=options)
 
