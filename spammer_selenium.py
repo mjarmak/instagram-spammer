3f3088106f36ba5b443f726b7b1e0aefaaafbd7f
mohamadjarmak@gmail.com <Mj314159265!>
2021-10-09 19:25:50 +0200

    fix

--- spammer_selenium.py
+++ spammer_selenium.py
@@ -48,7 +48,7 @@ options.add_experimental_option("mobileEmulation", mobile_emulation)
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--allow-running-insecure-content')
 options.add_argument("--disable-extensions")
